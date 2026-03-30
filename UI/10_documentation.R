b30cccc68bd2f4af5ad697a0ce83546db6fb26b3
Aurélie Fréchet <aurelie.frechet@gmail.com>
2026-03-30 21:55:42 +0200

    get rid of old version

--- UI/10_documentation.R
+++
@@ -1,4 +0,0 @@
-body_content[[length(body_content) + 1]] <-
-  tabItem("documentation",
-          "This is dashboard's page"
-  )
--- server.R
+++
@@ -1,8 +0,0 @@
-function(session, input, output){
-  
-  files_SERVER <- list.files("SERVER/")
-  for(f in file.path(getwd(), "SERVER", files_SERVER)){
-    source(f, local = TRUE)
-  }
-  
-}
